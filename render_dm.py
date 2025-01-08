250bb503ff4d2bf08fcdac49d58fb87ee2cae429
Florian Wetzels <wetzels@cs.uni-kl.de>
2025-01-08 17:38:03 +0100

    added rendering script

---
+++ render_dm.py
@@ -0,0 +1,27 @@
+import numpy as np
+from sklearn.manifold import TSNE
+from sklearn.manifold import MDS
+import matplotlib.pyplot as plt
+    
+   
+matrix0 = np.loadtxt(open(prepare-data/Cylinder2D.cdb/dm_la0, "rb"), delimiter=",", skiprows=1)
+matrix1 = np.loadtxt(open(prepare-data/Cylinder2D.cdb/dm_la1, "rb"), delimiter=",", skiprows=1)
+matrix2 = np.loadtxt(open(prepare-data/Cylinder2D.cdb/dm_la2, "rb"), delimiter=",", skiprows=1)
+
+fig, ax = plt.subplots()
+ax.imshow(matrix0, cmap='coolwarm', interpolation='nearest')
+ax.set_xticks(np.arange(0,301,25))
+ax.set_yticks(np.arange(0,301,25))
+fig.savefig("dm_la0.pdf", transparent=True, bbox_inches="tight", pad_inches=0.1)
+
+fig, ax = plt.subplots()
+ax.imshow(matrix1, cmap='coolwarm', interpolation='nearest')
+ax.set_xticks(np.arange(0,301,25))
+ax.set_yticks(np.arange(0,301,25))
+fig.savefig("dm_la1.pdf", transparent=True, bbox_inches="tight", pad_inches=0.1)
+
+fig, ax = plt.subplots()
+ax.imshow(matrix2, cmap='coolwarm', interpolation='nearest')
+ax.set_xticks(np.arange(0,301,25))
+ax.set_yticks(np.arange(0,301,25))
+fig.savefig("dm_la2.pdf", transparent=True, bbox_inches="tight", pad_inches=0.1)
